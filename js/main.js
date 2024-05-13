ea0644ce30539f086b36276a22fce2ba1ae12aa0
SardorQobilov <sardorqobilov555@gmail.com>
2024-05-13 19:15:02 +0500

    special__offers

--- js/main.js
+++ js/main.js
@@ -12,3 +12,15 @@ var swiper = new Swiper(".hero__swiper", {
   spaceBetween: 30,
   speed: 600,
 });
+
+// product__swiper
+var swiper = new Swiper(".product__swiper", {
+  navigation: {
+    nextEl: ".button-next",
+    prevEl: ".button-prev",
+  },
+  loop: true,
+  slidesPerView: 4,
+  spaceBetween: 32,
+  speed: 600,
+});
